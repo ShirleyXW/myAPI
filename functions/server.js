9cf5f402f1cf49e4cdc4aeded20e6a2ebee8683f
ShirleyXW <shirley.wang.xl@outlook.com>
2025-01-29 13:30:13 -0800

    add error control

--- functions/server.js
+++ functions/server.js
@@ -1,8 +1,30 @@
 const greeting = require("../modules/get-date");
 exports.handler = async (event, context) => {
-    const { queryStringParameters } = event;
+    if (event.path !== '/.netlify/functions/server'){
+      return {
+        statusCode: 400,
+        headers: {
+          'Content-Type': 'text/plain', 
+        },
+        body: 'Bad Request',
+    }
+  }
+
+  const { queryStringParameters } = event;
+  if (!queryStringParameters && !queryStringParameters.name){
+    return {
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'text/plain', 
+      },
+      body: 'Bad Request',
+    }
+  }
+
     const name = queryStringParameters.name || 'Anonymous';
     const message = greeting.getDate(name);
+
+
     return {
       statusCode: 200,
       headers: {
