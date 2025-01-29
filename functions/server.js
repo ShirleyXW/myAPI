386291cc483a5e8cedc324535f1372c48203f2a3
ShirleyXW <shirley.wang.xl@outlook.com>
2025-01-29 12:50:52 -0800

    change return type

--- functions/server.js
+++ functions/server.js
@@ -2,6 +2,11 @@ exports.handler = async (event, context) => {
     const { queryStringParameters } = event;
     const name = queryStringParameters.name || 'Anonymous';
   
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(`<h1>Hello ${name}</h1>`);
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html', 
+      },
+      body: `<h1>Hello ${name}</h1>`,
+    };
   };
